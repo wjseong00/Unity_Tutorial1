c51ad06227bdc69af7af80855a810743bfd9dc76
wonjin0217 <yssung021@gmail.com>
2020-05-23 01:53:45 +0900

    7. 라이트
    유니티 버전 :  Unity 2019.3.14f1 (64-bit)

---
+++ New Unity Project/Assets/Scenes/C# code/test6.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test6 : MonoBehaviour
+{
+
+    private Light theLight;
+
+    private float targetIntensity;
+    private float currentIntensity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theLight = GetComponent<Light>();
+        currentIntensity = theLight.intensity;
+        targetIntensity = Random.Range(0.4f, 1f);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(targetIntensity - currentIntensity) >=0.01)
+        {
+            if (targetIntensity - currentIntensity >= 0)
+            {
+                currentIntensity += Time.deltaTime * 3f;
+
+            }
+            else
+            {
+                currentIntensity -= Time.deltaTime * 3f;
+            }
+            theLight.intensity = currentIntensity;
+            theLight.range = currentIntensity+10;
+        }
+        else
+        {
+            targetIntensity = Random.Range(0.4f, 1f);
+        }
+    }
+}
