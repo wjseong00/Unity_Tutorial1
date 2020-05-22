639db596d233e02643cfa49be615ecab881a681e
wonjin0217 <yssung021@gmail.com>
2020-05-23 01:35:09 +0900

    6. 카메라
    유니티 버전 :  Unity 2019.3.14f1 (64-bit)

---
+++ New Unity Project/Assets/Scenes/C# code/test5.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test5 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject go_Target;
+    
+    [SerializeField]
+    private float speed;
+    
+    private Vector3 difValue;
+
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        difValue = transform.position - go_Target.transform.position;
+        difValue = new Vector3(Mathf.Abs(difValue.x),Mathf.Abs(difValue.y),Mathf.Abs(difValue.z));
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, go_Target.transform.position + difValue, speed);
+    }
+}
