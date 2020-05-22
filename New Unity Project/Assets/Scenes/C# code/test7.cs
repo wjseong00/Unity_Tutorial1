f96e5ee0a7fc77a59d06a668d5f8f98193c016ca
wonjin0217 <yssung021@gmail.com>
2020-05-23 02:06:33 +0900

    8. 애니메이션
    유니티 버전 :  Unity 2019.3.14f1 (64-bit)

---
+++ New Unity Project/Assets/Scenes/C# code/test7.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test7 : MonoBehaviour
+{
+    private Animation anim;
+
+    private Animation clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            //anim.Play("cube1");
+            //anim.PlayQueued("cube2");
+            anim.Blend("cube2");
+            //anim.CrossFade("cube2");
+            //if(!anim.IsPlaying("cube2"))
+            //{
+            //    anim.Play("cube2");
+            //}
+            //anim.Stop();
+            //
+            //anim.clip = clip;
+        }
+    }
+}
