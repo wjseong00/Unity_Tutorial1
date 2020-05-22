39db997dfe8d53fc83c719acf4d55f0a8e84852e
wonjin0217 <yssung021@gmail.com>
2020-05-22 23:46:56 +0900

    5. 메쉬 렌더러
    유니티 버전 :  Unity 2019.3.14f1 (64-bit)

---
+++ New Unity Project/Assets/Scenes/C# code/test4.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test4 : MonoBehaviour
+{
+    [SerializeField]
+    private Material red_Mat;
+    [SerializeField]
+    private Material green_Mat;
+
+    private MeshRenderer mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            mesh.material = green_Mat;
+        }
+        else
+        {
+            mesh.material = red_Mat;
+        }
+    }
+}
