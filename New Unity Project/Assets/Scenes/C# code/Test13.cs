8c2171c05751258f2fb360fdc48270ac9de02b92
wonjin0217 <yssung021@gmail.com>
2020-05-23 12:38:00 +0900

    15. 대화 시스템
    유니티 버전 :  Unity 2019.3.14f1 (64-bit)

--- New Unity Project/Assets/Scenes/C# code/Test13.cs
+++ New Unity Project/Assets/Scenes/C# code/Test13.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Test13 : MonoBehaviour
 {
     // Start is called before the first frame update
---
+++ New Unity Project/Assets/Scenes/C# code/Test14.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[System.Serializable]
+public class Dialogue
+{
+    [TextArea]
+    public string dialogue;
+    public Sprite cg;
+}
+
+public class Test14 : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer sprite_standingCG;
+    [SerializeField] private SpriteRenderer sprite_dialogueBox;
+    [SerializeField] private Text txt_Dialogue;
+
+    private bool isDialogue = false;
+
+    private int count = 0;
+
+    [SerializeField] private Dialogue[] dialogue;
+
+    public void ShowDialogue()
+    {
+        OnOff(true);
+        count = 0;
+        NextDialogue();
+    }
+   
+ 
+    private void OnOff(bool _flag)
+    {
+        sprite_dialogueBox.gameObject.SetActive(_flag);
+        sprite_standingCG.gameObject.SetActive(_flag);
+        txt_Dialogue.gameObject.SetActive(_flag);
+
+        isDialogue = _flag;
+    }
+
+   
+    private void NextDialogue()
+    {
+        txt_Dialogue.text = dialogue[count].dialogue;
+        sprite_standingCG.sprite = dialogue[count].cg;
+        count++;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(isDialogue)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                if(count < dialogue.Length)
+                {
+                    NextDialogue();
+                }
+                else
+                {
+                    OnOff(false);
+                }
+            }
+        }
+    }
+}
