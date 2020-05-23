9f6c5cddd3855f66ce0fc1d9c7810b3783d15d85
wonjin0217 <yssung021@gmail.com>
2020-05-23 11:23:13 +0900

    12. 슬라이더 UI
    유니티 버전 :  Unity 2019.3.14f1 (64-bit)

---
+++ New Unity Project/Assets/Scenes/C# code/Test11.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test11 : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+
+    private bool isClick;
+
+    private float dotTime = 1f;
+    private float currentDotTime = 0f;
+
+    private void Start()
+    {
+        
+          currentDotTime = dotTime;
+        
+    }
+
+    private void Update()
+    {
+        if(isClick)
+        {
+            currentDotTime -= Time.deltaTime;
+
+            if(currentDotTime<=0)
+            {
+                slider.value -= Time.deltaTime;
+                if(currentDotTime<=-1f)
+                {
+                    currentDotTime = dotTime;
+                }
+            }
+            
+        }
+       
+    }
+    public void Button()
+    {
+        isClick = true;
+    }
+
+
+}
