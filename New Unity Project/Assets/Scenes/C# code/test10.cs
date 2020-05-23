7628e07d6bd65a5841e31edb1ecc517faf4d466c
wonjin0217 <yssung021@gmail.com>
2020-05-23 11:05:47 +0900

    11. UI의 기본
    유니티 버전 :  Unity 2019.3.14f1 (64-bit)

---
+++ New Unity Project/Assets/Scenes/C# code/test10.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class test10 : MonoBehaviour
+{
+    [SerializeField] private Text txt_name;
+    [SerializeField] private Image img_name;\
+    [SerializeField] private Sprite sprite;
+
+    private bool isCoolTime = false;
+
+    private float currentTime = 5f;
+    private float delayTime = 5f;
+
+    private void Update()
+    {
+
+        //img_name.sprite = ;
+        Color color = img_name.color;
+        color.a = 0f;
+        img_name.color = color;
+
+        if(isCoolTime)
+        {
+            currentTime -= Time.deltaTime;
+            img_name.fillAmount = currentTime /delayTime;
+            if(currentTime <=0)
+            {
+                isCoolTime = false;
+                currentTime = delayTime;
+                img_name.fillAmount = currentTime;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    public void Change()
+    {
+        txt_name.text = "변경됨";
+        isCoolTime = true;
+        //img_name.fillAmount = 0.5f;
+    }
+}
