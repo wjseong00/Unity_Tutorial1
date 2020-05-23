445cde1a7aa72dffd1519111efb535bca03cf964
wonjin0217 <yssung021@gmail.com>
2020-05-23 12:47:04 +0900

    16. 오디오 소스
    유니티 버전 :  Unity 2019.3.14f1 (64-bit)

---
+++ New Unity Project/Assets/Scenes/C# code/Test15.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test15 : MonoBehaviour
+{
+    private AudioSource theAudio;
+
+    [SerializeField] private AudioClip[] clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theAudio = GetComponent<AudioSource>();
+    }
+    public void PlaySE()
+    {
+        int _temp = Random.Range(0, 4);
+        theAudio.clip = clip[_temp];
+        theAudio.Play();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
