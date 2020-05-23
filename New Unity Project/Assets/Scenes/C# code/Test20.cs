091759c22e92c15412389b0e35282bbd2a6577a4
wonjin0217 <yssung021@gmail.com>
2020-05-23 19:46:24 +0900

    25. 모바일 D 패드
    유니티 버전 :  Unity 2019.3.14f1 (64-bit)

---
+++ New Unity Project/Assets/Scenes/C# code/Test20.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class Test20 : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    [SerializeField] private RectTransform rect_Background;
+    [SerializeField] private RectTransform rect_Joystick;
+
+    private float radius;
+
+    [SerializeField] private GameObject go_Player;
+    [SerializeField] private float moveSpeed;
+
+    private bool isTouch = false;
+    private Vector3 movePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = rect_Background.rect.width * 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTouch)
+        {
+            go_Player.transform.position += movePosition;
+        }
+            
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 value = eventData.position - (Vector2)rect_Background.position;
+
+        value = Vector2.ClampMagnitude(value, radius);
+        rect_Joystick.localPosition = value;
+
+
+        float distance = Vector2.Distance(rect_Background.position, rect_Joystick.position) / radius;
+        value = value.normalized;
+        movePosition = new Vector3(value.x * moveSpeed * Time.deltaTime*distance, 0f, value.y * moveSpeed * Time.deltaTime* distance);
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isTouch = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isTouch = false;
+        rect_Joystick.localPosition = Vector3.zero;
+        movePosition = Vector3.zero;
+    }
+}
